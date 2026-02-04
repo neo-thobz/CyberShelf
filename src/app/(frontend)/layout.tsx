33f945f2d5000ba5ad63f3b93c97c22c11be4cf0
neoThobs <neo.thobz@gmail.com>
2026-02-04 20:23:29 +0200

    chore: configure tailwind css

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -1,10 +1,5 @@
 import React from 'react'
-import './styles.css'
-
-export const metadata = {
-  description: 'A blank template using Payload in a Next.js app.',
-  title: 'Payload Blank Template',
-}
+import './globals.css'
 
 export default async function RootLayout(props: { children: React.ReactNode }) {
   const { children } = props
